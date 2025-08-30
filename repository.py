398187b960395853ed256154bc0cca6ee01eb230
Raphael Ramos <raphael.opensource@gmail.com>
2025-08-30 18:17:25 -0300

    test: get batch by reference

--- repository.py
+++ repository.py
@@ -1,6 +1,8 @@
 import abc
 import model
 
+from sqlalchemy.sql import text
+
 
 class AbstractRepository(abc.ABC):
     @abc.abstractmethod
@@ -17,9 +19,32 @@ class SqlRepository(AbstractRepository):
         self.session = session
 
     def add(self, batch):
-        # self.session.execute('INSERT INTO ??
+        # Insert batch if it does not exist
+        # Get lines IDs on _allocations
+        # Insert tuple (batch_id, order_id) on allocations only if it does not exist
         ...
 
     def get(self, reference) -> model.Batch:
-        # self.session.execute('SELECT ??
-        ...
+        [[reference, sku, _purchased_quantity, eta]] = self.session.execute(
+            text(
+                "SELECT reference, sku, _purchased_quantity, eta FROM batches"
+                " WHERE reference=:reference"
+            ),
+            dict(reference=reference),
+        )
+        batch = model.Batch(reference, sku=sku, qty=_purchased_quantity, eta=eta)
+
+        rows = list(
+            self.session.execute(
+                text(
+                    "SELECT ol.orderid, ol.sku, ol.qty FROM order_lines AS ol"
+                    " JOIN batches AS b ON b.sku = ol.sku"
+                    " WHERE b.reference=:reference"
+                ),
+                dict(reference=batch.reference),
+            )
+        )
+        batch._allocations = set(
+            model.OrderLine(orderid=row[0], sku=row[1], qty=row[2]) for row in rows
+        )
+        return batch
